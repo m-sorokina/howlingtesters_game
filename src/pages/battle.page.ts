2ca30a5bcb78ef9f2b0aa5cbff68d01d77272905
m-sorokin <pdtt.lm@gmail.com>
2026-08-10 08:37:25 +0200

    feat: update BattlePage URL to use routes constant for consistency

--- src/pages/battle.page.ts
+++ src/pages/battle.page.ts
@@ -1,8 +1,9 @@
 import { BasePage } from '@pages';
 import type { Page } from 'playwright-core';
+import { routes } from '@consts/routes';
 
 export class BattlePage extends BasePage {
-  url = '/party-fight/';
+  url = routes.battle;
 
   constructor(page: Page) {
     super(page);
